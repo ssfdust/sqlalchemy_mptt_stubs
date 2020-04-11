fde64d49cc5c15c434707d31a0942aac1dfa337e
ssfdust <ssfdust@gmail.com>
2020-04-11 20:29:03 +0800

    add readme

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name="sqlalchemy-mptt stubs",
-    url=None,
+    name="sqlalchemy-mptt-stubs",
+    url="https://github.com/ssfdust/sqlalchemy_mptt_stubs",
     author='Neng Ming',
     author_email='ssfdust@gmail.com',
-    version="0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.2",
     package_dir={'': 'package'},
     package_data={"sqlalchemy_mptt": ["__init__.pyi", "events.pyi", "mixins.pyi"]},
     packages=[
